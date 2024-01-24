ae85f6de58ee732b34a4b8aba854e3a6f1f61299
Pavel <Pavel.Pasechnik@gmail.com>
2024-01-24 16:17:35 +0200

    created the structure

---
+++ src/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import('./js/home-quote.js');
+import('./js/exercises.js');
+import('./js/motivation.js');
+import('./js/footer.js');
+import('./js/favorites-quote.js');
+import('./js/favorites-section.js');
+import('./js/menu.js');
+import('./js/modal.js');
+import('./js/modal-rating.js');
