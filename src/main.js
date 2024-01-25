94031d70edc7a5b1d3894c638182f2dad4da3012
Pavel <Pavel.Pasechnik@gmail.com>
2024-01-25 11:07:50 +0200

    added style for container fnd files for scroll up

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@
 
 import('./js/home-quote.js');
 import('./js/exercises.js');
-import('./js/footer.js');
 import('./js/favorites-quote.js');
 import('./js/favorites-section.js');
+import('./js/footer.js');
+import('./js/scroll-up.js');
 import('./js/menu.js');
 import('./js/modal.js');
 import('./js/modal-rating.js');
