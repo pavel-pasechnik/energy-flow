ccb8072417aff99af564218d8f4e750202e36336
dmitrsavit <141586035+ex1kenny@users.noreply.github.com>
2024-01-27 18:11:02 +0200

    header6

--- src/js/header.js
+++ src/js/header.js
@@ -15,6 +15,38 @@
 
 // onload();
 
+// second current page
+
+// function onload() {
+//   const lnk = document.querySelectorAll('.navigation-link');
+//   const pathName = window.location.pathname;
+
+//   for (let i = 0; i < lnk.length; i++) {
+//     const currentHref = lnk[i].pathname;
+
+//     if (
+//       (pathName === '/' && i === 0) ||
+//       (pathName !== '/' && currentHref === pathName)
+//     ) {
+//       lnk[i].classList.add('current-page-link');
+//     }
+//   }
+// }
+
+// onload();
+
+// add currnet link
+document.addEventListener('DOMContentLoaded', onload);
+
+if (
+  document.readyState === 'complete' ||
+  (document.readyState !== 'loading' && !document.documentElement.doScroll)
+) {
+  onload();
+} else {
+  document.addEventListener('DOMContentLoaded', onload);
+}
+
 function onload() {
   const lnk = document.querySelectorAll('.navigation-link');
   const pathName = window.location.pathname;
@@ -30,5 +62,3 @@ function onload() {
     }
   }
 }
-
-onload();
