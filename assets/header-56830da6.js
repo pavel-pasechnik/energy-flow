d2aa25f9a9739e6273cf15fdd35c56db956c17e5
pavel-pasechnik <Pavel.Pasechnik@gmail.com>
2024-01-27 08:45:44 +0000

    Deploying to gh-pages from @ pavel-pasechnik/energy-flow@886ba2ae3ace91c088a97bee84166d8a68894ecd 🚀

--- assets/header-56830da6.js
+++
@@ -1,2 +0,0 @@
-function t(){const l=document.querySelectorAll(".navigation-link");for(let n=0;n<l.length;n++)l[n].href==document.URL.split(/[\?#]/)[0]&&l[n].classList.add("current-page-link")}t();
-//# sourceMappingURL=header-56830da6.js.map
---
+++ assets/header-a119847e.js
@@ -0,0 +1,2 @@
+function o(){const t=document.querySelectorAll(".navigation-link"),a=window.location.pathname;for(let n=0;n<t.length;n++){const e=t[n].pathname;(a==="/"&&n===0||e===a)&&t[n].classList.add("current-page-link")}}o();
+//# sourceMappingURL=header-a119847e.js.map
