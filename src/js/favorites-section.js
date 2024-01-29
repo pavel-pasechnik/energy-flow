d0d79aca853895c8e9ceb619614a23c932acf50c
NovichokUA <zubr7333@gmail.com>
2024-01-29 03:20:05 +0200

    add error js, fix render card, add png dumbbell

--- src/js/favorites-section.js
+++ src/js/favorites-section.js
@@ -1,33 +1,35 @@
-
 import { getRequest } from './api-energy-flow';
 
 const ulFavoritesList = document.querySelector('.favorites-list-item');
-
-const savedFavoritesExercises = JSON.parse(localStorage.getItem('favorites'));
-
+const containerErrorMasseg = document.querySelector(
+  '.favorites-container-error-notification'
+);
 // функція отримання массиву вправ з localStorage
-async function getFavoritseList() {
-  let arrayData = [];
+// async function getFavoritseList() {
+//   let arrayData = [];
 
-  for (const _id of savedFavoritesExercises) {
-    await getRequest(`/exercises/${_id}`)
-      .then(data => {
-        return arrayData.push(data);
-      })
-      .catch(error => console.log(error));
-  }
-  return arrayData;
-}
+//   for (const _id of savedFavoritesExercises) {
+//     await getRequest(`/exercises/${_id}`)
+//       .then(data => {
+//         return arrayData.push(data);
+//       })
+//       .catch(error => console.log(error));
+//   }
+
+//   return arrayData;
+// }
 
 // пагінация по сторінкам за допомогою кнопочок
 
 async function renderFavoritseList() {
-  let arrayData = await getFavoritseList();
-  const perPage = 8;
-  let currentPage = 1;
-
   // функція для показу сторінки за значенням perPage
   function check() {
+    let arrayData = JSON.parse(localStorage.getItem('favorites'));
+    console.log(arrayData);
+    renderErrorCard(arrayData);
+    const perPage = 8;
+    let currentPage = 1;
+
     if (window.matchMedia('(max-width: 376px)').matches) {
       function favoritesList(arrayData, perPage, currentPage) {
         ulFavoritesList.innerHTML = '';
@@ -91,24 +93,35 @@ async function renderFavoritseList() {
     } else {
       ulFavoritesList.innerHTML = cardMarking(arrayData);
     }
+
+    ulFavoritesList.addEventListener('click', event => {
+      const element = event.target;
+      if (element.classList.contains('favorites-btn-trash')) {
+        let i = element.dataset.id;
+        const b = arrayData.filter(id => id._id !== i);
+        let c = localStorage.setItem('favorites', JSON.stringify(b));
+        console.log(i);
+        check();
+      }
+    });
   }
   check();
-  window.addEventListener('resize', check);
+  // window.addEventListener('resize', check);
 }
 
 // функція рендеру картки
 
 function cardMarking(obj) {
   return obj.reduce(
-    (html, { bodyPart, burnedCalories, target, time, equipment }) =>
+    (html, { bodyPart, burnedCalories, target, time, equipment, _id }) =>
       html +
       `<li class="favorits-item">
           <div class="favorites-container-nav">
             <div class="favorites-container-workout">
               <p class="favoritese-title">WORKOUT</p>
-              <button type="button" class="favorites-btn-trash">
+              <button type="button" class="favorites-btn-trash" data-id="${_id}" ">
                 <svg width="16" height="16" class="favorites-trash">
-                  <use href="./img/symbol-defs.svg#icon-trash-favorites"></use>
+                  <use href="../img/sprite.svg#favorites-trash"></use>
                 </svg>
               </button>
             </div>
@@ -116,16 +129,16 @@ function cardMarking(obj) {
               <p class="favorites-title-btn-start">Start</p>
               <button type="button" class="favorites-btn-arrow">
                 <svg class="favorites-arrow" width="14" height="14" alt="arrow">
-                  <use href="../img/favorites-arrow.svg"></use>
+                  <use href="../img/sprite.svg#favorites-arrow"></use>
                 </svg>
               </button>
             </div>
           </div>
           <div class="favorite-title-exercises">
             <svg width="24" height="24" class="favorites-icon-man">
-              <use href="./img/symbol-defs.svg#icon-icon-favorites"></use>
+              <use href="../img/sprite.svg#favorites-man"></use>
             </svg>
-            <p class="favorites-title-name">${equipment}</p>
+            <p class="favorites-title-name">${target}</p>
           </div>
           <div class="favorites-description-workout">
             <p class="favorites-title-descriptoin">
@@ -140,12 +153,40 @@ function cardMarking(obj) {
             </p>
             <p class="favorites-title-descriptoin">
               <span class="favorites-description-workout-span">Target:</span>
-              ${target}
+              ${equipment}
             </p>
           </div>
         </li>`,
     ''
   );
+}
+
+function renderErrorCard(array) {
+  if (array.length === 0) {
+    return (containerErrorMasseg.innerHTML = `<h2 class="favorites-container-error-title">Favorites</h2>
+    <div class="favorites-container-error-description">
+      <img
+        srcset="
+          ../img/favorites-section/mobile-dumbbell.png             85w,
+          ../img/favorites-section/mobile-dumbbell@2x.png         170w,
+          ../img/favorites-section/tablet-desctop-dumbbell.png    116w,
+          ../img/favorites-section/tablet-desctop-dumbbell@2x.png 116w
+        "
+        sizes="
+      (min-width: 1440px) 116px, 
+      (min-width: 768px) 116px, 
+      (max-width: 767px) 85px"
+        src="../img/favorites-section/mobile-dumbbell.png"
+        alt="dumbbell"
+      />
+
+      <p class="favorites-error-description">
+        It appears that you haven't added any exercises to your favorites yet.
+        To get started, you can add exercises that you like to your favorites
+        for easier access in the future.
+      </p>
+    </div>`);
+  }
 }
 
 renderFavoritseList();
