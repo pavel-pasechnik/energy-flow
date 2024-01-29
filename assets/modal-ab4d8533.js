dfd0f01edebc7dbb09345fc9c751c4903ebee9f5
pavel-pasechnik <Pavel.Pasechnik@gmail.com>
2024-01-29 05:54:01 +0000

    Deploying to gh-pages from @ pavel-pasechnik/energy-flow@3a559a0f3771dfc5aa78fd1928194c4da17613c9 🚀

--- assets/modal-ab4d8533.js
+++ assets/modal-ab4d8533.js
@@ -1,7 +1,7 @@
 import{getRequest as v}from"./api-energy-flow-3175d380.js";import"./vendor-cae18210.js";document.addEventListener("click",e=>{if(e.target.classList.contains("exercises-gallery-btn-start")){const s=e.target.dataset.action;u(s)}});function u(e){v(`/exercises/${e}`).then(s=>{f(s)})}function f(e){const s=document.getElementById("exerciseModal"),t=document.getElementById("exerciseDetails");t.innerHTML="";const o=document.createElement("div");o.innerHTML=`
     <div class="exercise-details">
       <button id="closeButton" class="close-button">
-    <svg class="close-icon" fill="#1B1B1B">
+    <svg class="close-icon">
       <use href="./img/sprite.svg#close"></use>
     </svg>
   </button>
@@ -16,7 +16,7 @@ import{getRequest as v}from"./api-energy-flow-3175d380.js";import"./vendor-cae18
               <h2 class="exercise-name">${e.name}</h2>
               <div class="star-container">
                 <span class="rating-value">${p(e.rating)}</span>
-                <div class="star-rating">${g(e.rating)}</div>
+                <div class="star-rating">${m(e.rating)}</div>
               </div>
               <div class="info-block">
                 <hr class="section-divider-top">
@@ -37,16 +37,16 @@ import{getRequest as v}from"./api-energy-flow-3175d380.js";import"./vendor-cae18
       <use href="./img/sprite.svg#heart"></use>
     </svg>
   </button>
-                <button class="rating-button rate-button">Give a Rating</button>
+                
               </div>
             </div>
         </div>
       </div>
-    </div>`,t.appendChild(o),s.classList.add("is-visible");const n=document.getElementById("favoriteButton");c(e._id)&&l(n),n.addEventListener("click",()=>{c(e._id)?(b(e._id),h(n)):(m(e._id),l(n))});const r=document.getElementById("exerciseModal");r.addEventListener("click",i=>{(i.target===r||i.target.closest("#closeButton"))&&d()})}function p(e){return isNaN(e)?"N/A":Math.round(e).toFixed(1)}function g(e){const s=Math.round(e),t=5-s,o=`<svg class="star">
+    </div>`,t.appendChild(o),s.classList.add("is-visible");const n=document.getElementById("favoriteButton");c(e._id)&&l(n),n.addEventListener("click",()=>{c(e._id)?(h(e._id),y(n)):(g(e._id),l(n))});const r=document.getElementById("exerciseModal");r.addEventListener("click",i=>{(i.target===r||i.target.closest("#closeButton"))&&d()})}function p(e){return isNaN(e)?"N/A":Math.round(e).toFixed(1)}function m(e){const s=Math.round(e),t=5-s,o=`<svg class="star">
     <use href="./img/sprite.svg#colorstar"></use>
 </svg>`,n=`
-  <svg class="star">
+  <svg class="star empty-star">
     <use href="./img/sprite.svg#star"></use>
   </svg>
-`;let r="";for(let i=0;i<s;i++)r+=o;for(let i=0;i<t;i++)r+=n;return r}function c(e){return a().includes(e)}function a(){return JSON.parse(localStorage.getItem("favorites"))||[]}function m(e){const s=a();s.push(e),localStorage.setItem("favorites",JSON.stringify(s))}function b(e){const t=a().filter(o=>o!==e);localStorage.setItem("favorites",JSON.stringify(t))}function l(e){const s=e.querySelector("span");s.textContent="Remove from",e.querySelector("use").setAttribute("href","./img/sprite.svg#heart")}function h(e){const s=e.querySelector("span");s.textContent="Add to Favorites",e.querySelector("use").setAttribute("href","./img/sprite.svg#heart")}function d(){console.log("Close button clicked");const e=document.getElementById("exerciseModal");e&&e.classList.remove("is-visible")}document.addEventListener("keydown",e=>{e.key==="Escape"&&d()});
-//# sourceMappingURL=modal-78645e02.js.map
+`;let r="";for(let i=0;i<s;i++)r+=o;for(let i=0;i<t;i++)r+=n;return r}function c(e){return a().includes(e)}function a(){return JSON.parse(localStorage.getItem("favorites"))||[]}function g(e){const s=a();s.push(e),localStorage.setItem("favorites",JSON.stringify(s))}function h(e){const t=a().filter(o=>o!==e);localStorage.setItem("favorites",JSON.stringify(t))}function l(e){const s=e.querySelector("span");s.textContent="Remove from",e.querySelector("use").setAttribute("href","./img/sprite.svg#heart")}function y(e){const s=e.querySelector("span");s.textContent="Add to Favorites",e.querySelector("use").setAttribute("href","./img/sprite.svg#heart")}function d(){console.log("Close button clicked");const e=document.getElementById("exerciseModal");e&&e.classList.remove("is-visible")}document.addEventListener("keydown",e=>{e.key==="Escape"&&d()});
+//# sourceMappingURL=modal-ab4d8533.js.map
