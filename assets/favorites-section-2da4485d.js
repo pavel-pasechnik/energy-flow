847c1df4cee9c7522c3cc7dd31d257d0b5f584a8
pavel-pasechnik <Pavel.Pasechnik@gmail.com>
2024-01-29 17:01:34 +0000

    Deploying to gh-pages from @ pavel-pasechnik/energy-flow@f85f0956979cf3d7b052331f13ed6db6ea7171bf 🚀

--- assets/favorites-section-2da4485d.js
+++ assets/favorites-section-2da4485d.js
@@ -1,8 +1,8 @@
-import"./api-energy-flow-3175d380.js";import"./vendor-cae18210.js";const p=document.querySelector(".favorites-list-item"),m=document.querySelector(".favorites-container-error-notification");async function b(){function o(){let e=JSON.parse(localStorage.getItem("favorites"));console.log(e),h(e);const l=8;let s=1;if(window.matchMedia("(max-width: 376px)").matches){let n=function(i,t,a){p.innerHTML="",a--;const f=t*a,v=f+t,d=i.slice(f,v);p.innerHTML=u(d)},r=function(i,t){const a=document.querySelector(".pagination"),f=Math.ceil(i.length/t),v=document.createElement("ul");v.classList.add("favorites-pagination-container-btn");for(let d=0;d<f;d++){const g=c(d+1);v.appendChild(g)}a.appendChild(v)},c=function(i){const t=document.createElement("li");return t.innerText=i,s==i&&t.classList.add("favorites-pagination-btn"),s!==i&&t.classList.add("favorites-pagination-btn-not-activ"),t.addEventListener("click",()=>{s=i,n(e,l,s);let a=document.querySelector("li.favorites-pagination-btn");a.classList.remove("favorites-pagination-btn"),a.classList.add("favorites-pagination-btn-not-activ"),t.classList.remove("favorites-pagination-btn-not-activ"),t.classList.add("favorites-pagination-btn")}),t};n(e,l,s),r(e,l)}else p.innerHTML=u(e);p.addEventListener("click",n=>{const r=n.target;if(r.classList.contains("favorites-btn-trash")){let c=r.dataset.id;const i=e.filter(t=>t._id!==c);localStorage.setItem("favorites",JSON.stringify(i)),console.log(c),o()}})}o()}function u(o){return o.reduce((e,{bodyPart:l,burnedCalories:s,target:n,time:r,equipment:c,_id:i})=>e+`<li class="favorits-item">
+import"./api-energy-flow-3175d380.js";import"./vendor-cae18210.js";const f=document.querySelector(".favorites-list-item"),b=document.querySelector(".favorites-container-error-notification"),g=document.querySelector(".favorites-main"),u=document.querySelector(".favorites-pagination-container-btn");g.classList.remove("is-hidden");async function w(){function n(){let i=JSON.parse(localStorage.getItem("favorites"));if(i.length===0||i===null)return g.classList.add("is-hidden"),h();console.log(i);const l=8;let s=1;if(window.matchMedia("(max-width: 376px)").matches){let o=function(e,t,a){f.innerHTML="",a--;const v=t*a,d=v+t,p=e.slice(v,d);f.innerHTML=m(p)},r=function(e,t){u.innerHTML="";const a=document.querySelector(".pagination"),v=Math.ceil(e.length/t);for(let d=0;d<v;d++){const p=c(d+1);u.appendChild(p)}a.appendChild(u)},c=function(e){const t=document.createElement("li");return t.innerText=e,s==e&&t.classList.add("favorites-pagination-btn"),s!==e&&t.classList.add("favorites-pagination-btn-not-activ"),t.addEventListener("click",()=>{s=e,o(i,l,s);let a=document.querySelector("li.favorites-pagination-btn");a.classList.remove("favorites-pagination-btn"),a.classList.add("favorites-pagination-btn-not-activ"),t.classList.remove("favorites-pagination-btn-not-activ"),t.classList.add("favorites-pagination-btn")}),t};o(i,l,s),r(i,l)}else f.innerHTML=m(i);f.addEventListener("click",o=>{const r=o.target;if(i.length===0||i===null)return g.classList.add("is-hidden"),h();if(r.classList.contains("favorites-btn-trash")){let c=r.dataset.id;const e=i.filter(t=>t._id!==c);localStorage.setItem("favorites",JSON.stringify(e)),console.log(c),n()}})}n(),window.addEventListener("resize",n)}function m(n){return n.reduce((i,{bodyPart:l,burnedCalories:s,target:o,time:r,equipment:c,_id:e})=>i+`<li class="favorits-item">
           <div class="favorites-container-nav">
             <div class="favorites-container-workout">
               <p class="favoritese-title">WORKOUT</p>
-              <button type="button" class="favorites-btn-trash" data-id="${i}" ">
+              <button type="button" class="favorites-btn-trash" data-id="${e}" ">
                 <svg width="16" height="16" class="favorites-trash">
                   <use href="../img/sprite.svg#favorites-trash"></use>
                 </svg>
@@ -21,7 +21,7 @@ import"./api-energy-flow-3175d380.js";import"./vendor-cae18210.js";const p=docum
             <svg width="24" height="24" class="favorites-icon-man">
               <use href="../img/sprite.svg#favorites-man"></use>
             </svg>
-            <p class="favorites-title-name">${n}</p>
+            <p class="favorites-title-name">${o}</p>
           </div>
           <div class="favorites-description-workout">
             <p class="favorites-title-descriptoin">
@@ -39,7 +39,7 @@ import"./api-energy-flow-3175d380.js";import"./vendor-cae18210.js";const p=docum
               ${c}
             </p>
           </div>
-        </li>`,"")}function h(o){if(o.length===0)return m.innerHTML=`<h2 class="favorites-container-error-title">Favorites</h2>
+        </li>`,"")}function h(n){return b.innerHTML=`<h2 class="favorites-container-error-title">Favorites</h2>
     <div class="favorites-container-error-description">
       <img
         srcset="
@@ -61,5 +61,5 @@ import"./api-energy-flow-3175d380.js";import"./vendor-cae18210.js";const p=docum
         To get started, you can add exercises that you like to your favorites
         for easier access in the future.
       </p>
-    </div>`}b();
-//# sourceMappingURL=favorites-section-561b5e14.js.map
+    </div>`}w();
+//# sourceMappingURL=favorites-section-2da4485d.js.map
