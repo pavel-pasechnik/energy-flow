f6b10f0e550181eac6694e54f009365e27a97c56
pavel-pasechnik <Pavel.Pasechnik@gmail.com>
2024-01-31 06:40:55 +0000

    Deploying to gh-pages from @ pavel-pasechnik/energy-flow@cba8ac08dca326db1912f76fce3f604fcb03b58c 🚀

--- assets/exercises-f5f865f4.js
+++ assets/exercises-f5f865f4.js
@@ -22,13 +22,13 @@ import{getRequest as q}from"./api-energy-flow-8bc8b845.js";import"./vendor-afb53
       <p class="badge">WORKOUT</p>
       <label class="exercises-gallery-raiting">${t.rating}</label
       ><svg class="exercises-gallery-raiting-svg" width="14" height="13">
-        <use href="../sprite.svg#star"></use>
+        <use href="./sprite.svg#star"></use>
       </svg>
     </div>
     <button class="exercises-gallery-btn-start" data-action="${t._id}" type="button">
       Start
       <svg class="exercises-gallery-btn-icon">
-        <use href="../sprite.svg#"></use>
+        <use href="./sprite.svg#favorites-arrow"></use>
       </svg>
     </button>
   </div>
@@ -57,4 +57,4 @@ import{getRequest as q}from"./api-energy-flow-8bc8b845.js";import"./vendor-afb53
     </li>
   </ul>
 </a>`,"");if(u.innerHTML=T,i>1){const n=E(r,i);l.innerHTML=n,l.removeEventListener("click",t=>{t.target.tagName==="BUTTON"&&(e.page=parseInt(t.target.textContent),callback(e),e.page=1)})}else l.innerHTML=""}).catch(s=>{console.log(s)})};const c=document.querySelector(".subspecies"),h=document.querySelector(".exercises-nav-panel"),u=document.querySelector(".exercises-gallery"),$=document.querySelector(".exercises-nav-input"),w=document.querySelector(".exercises-nav-input-block"),B=document.querySelector(".input-search-icon"),l=document.querySelector(".pagination"),v=document.querySelector(".exercises-container"),S=document.querySelector(".exercises-current-ex"),U=document.querySelector(".without-exercises");let k=1,d="Muscles",o,C=window.innerWidth,g=0,p=0,m="";C<=375?(g=8,p=8):C<=768?(g=12,p=8):(g=12,p=9);const b={filter:d,page:k,limit:g},a={page:k,limit:p};f(b),h.addEventListener("click",e=>{if(e.target.tagName==="BUTTON")Array.from(e.currentTarget.children).map(s=>{s.textContent!==e.target.textContent?s.classList.remove("exercises-button-isactive"):e.target.classList.add("exercises-button-isactive")}),S.textContent="",d=e.target.textContent.trim(),b.filter=d,delete a[o],v.scrollIntoView({behavior:"smooth"}),f(b),L(b,f),c.classList.remove("is-hidden"),u.classList.add("is-hidden"),w.classList.add("is-hidden");else return}),c.addEventListener("click",e=>{e.preventDefault(),e.target.tagName==="BUTTON"&&(m=e.target.dataset.value,o=d.toLowerCase(),o==="body parts"&&(o="bodypart"),S.textContent=m.charAt(0).toUpperCase()+m.slice(1),a[o]=m,c.classList.add("is-hidden"),u.classList.remove("is-hidden"),w.classList.remove("is-hidden"),v.scrollIntoView({behavior:"smooth"}),x(a),L(a,x))}),B.addEventListener("click",e=>{a.keyword=$.value.trim(),x(a),L(a,x),delete a.keyword}),N()}
-//# sourceMappingURL=exercises-bf472a49.js.map
+//# sourceMappingURL=exercises-f5f865f4.js.map
