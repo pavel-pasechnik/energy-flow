6c71fa53899fb832abe6399fec29d7c5ce5a73e3
pavel-pasechnik <Pavel.Pasechnik@gmail.com>
2024-01-31 17:48:47 +0000

    Deploying to gh-pages from @ pavel-pasechnik/energy-flow@1ac759fc095da2a2498c7c41a7356d16335be90a 🚀

---
+++ assets/loader-40ff4a88.js
@@ -0,0 +1,2 @@
+const d=()=>{const e=document.createElement("div");e.classList.add("container-loader"),e.insertAdjacentHTML("afterbegin",'<div class="loader"></div>')},o=()=>{document.querySelector(".loader").remove()};export{d as addLoading,o as removeLoading};
+//# sourceMappingURL=loader-40ff4a88.js.map
