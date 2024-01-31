9a243ae30e8ed7688520e775dc7bbaf49e6e4232
pavel-pasechnik <Pavel.Pasechnik@gmail.com>
2024-01-31 11:47:13 +0000

    Deploying to gh-pages from @ pavel-pasechnik/energy-flow@40f0ef299501c6a1378787b87b71298e004ccac6 🚀

--- assets/modal-d33a74cb.js
+++ assets/modal-d33a74cb.js
@@ -43,10 +43,10 @@ import{getRequest as m}from"./api-energy-flow-8bc8b845.js";import{s as r}from"./
           </div>
         </div>
       </div>
-    </div>`,i.appendChild(a),t.classList.add("is-visible");const o=document.getElementById("favoriteButton");v(e._id)&&u(o),o.addEventListener("click",()=>{v(e._id)?(y(e._id),E(o)):(b(e),u(o))}),s.addEventListener("click",c=>{c.stopPropagation(),l()}),document.getElementById("closeButton").addEventListener("click",()=>{l()}),document.addEventListener("keydown",c=>{c.key==="Escape"&&l()})}function g(e){return isNaN(e)?"N/A":Math.round(e).toFixed(1)}function h(e){const t=Math.round(e),s=5-t,i=`<svg class="star" width="18" height="18" >
+    </div>`,i.appendChild(a),t.classList.add("is-visible");const o=document.getElementById("favoriteButton");v(e._id)&&u(o),o.addEventListener("click",()=>{v(e._id)?(y(e._id),E(o)):(b(e),u(o))}),s.addEventListener("click",c=>{c.stopPropagation(),l()}),document.getElementById("closeButton").addEventListener("click",()=>{l()}),document.addEventListener("keydown",c=>{c.key==="Escape"&&l()})}function g(e){return isNaN(e)?"N/A":e.toFixed(1)}function h(e){const t=Math.round(e),s=5-t,i=`<svg class="star" width="18" height="18" >
     <use href="${r}#colorstar"></use>
   </svg>`,a=`
     <svg class="star empty-star" width="18" height="18" >
       <use href="${r}#star"></use>
     </svg>`;let o="";for(let n=0;n<t;n++)o+=i;for(let n=0;n<s;n++)o+=a;return o}function v(e){return d().some(s=>s._id===e)}function d(){return JSON.parse(localStorage.getItem("favorites"))||[]}function b(e){const t=d();t.push(e),localStorage.setItem("favorites",JSON.stringify(t))}function y(e){const s=d().filter(i=>i._id!==e);s.length>0?localStorage.setItem("favorites",JSON.stringify(s)):localStorage.removeItem("favorites")}function u(e){const t=e.querySelector("span");t.textContent="Remove from",e.querySelector("use").setAttribute("href",`${r}#heart`)}function E(e){const t=e.querySelector("span");t.textContent="Add to Favorites",e.querySelector("use").setAttribute("href",`${r}#heart`)}document.addEventListener("click",e=>{const t=e.target;if(t.classList.contains("exercises-gallery-btn-start")||t.classList.contains("favorites-btn-arrow")){const s=t.dataset.action;f(s,t.classList.contains("exercises-gallery-btn-start"))}});
-//# sourceMappingURL=modal-4009712e.js.map
+//# sourceMappingURL=modal-d33a74cb.js.map
